4407746c10b6d43beed5a18158f13cfe85913568
mifth <mifth@mifth-System-Product-Name>
2012-01-03 18:06:47 -0500

    Scene Composer - support for emission, specular, Composite materials, Ambient Occlusion,

--- dev/trunk/TechDemo/src/com/bigboots/scene/SceneComposer.java
+++ dev/trunk/TechDemo/src/com/bigboots/scene/SceneComposer.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2011  BigBoots Team
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * See <http://www.gnu.org/licenses/>.
  */
 package com.bigboots.scene;
 
@@ -8,6 +19,7 @@ package com.bigboots.scene;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.*;
 import com.jme3.scene.*;
+import com.jme3.util.TangentBinormalGenerator;
 import java.io.File;
 
 
@@ -19,13 +31,15 @@ public class SceneComposer {
     
 private AssetManager assett;
 private Node sc;    
-private File dir;
+private String dirbase;
+private String dirlevel;
     
-  public  SceneComposer (Node scene, String texFolder, AssetManager assetM) {
+  public  SceneComposer (Node scene, String dirTexBase, String dirTexLevel, AssetManager assetM) {
     
         assett = assetM;
         sc = scene;
-        dir = new File(texFolder);                  
+        dirbase = dirTexBase;    
+        dirlevel = dirTexLevel;
         
     }
     
@@ -42,7 +56,7 @@ public void composeMaterial() {
             
                 Geometry geom_sc = (Geometry) spatial;
                 System.out.println("Composing Material: " + geom_sc.getMaterial().getName() + " for Geometry " + geom_sc.getName());
-                MaterialComposer matComp = new MaterialComposer(geom_sc, dir, assett);
+                MaterialComposer matComp = new MaterialComposer(geom_sc, dirbase, dirlevel, assett);
                 matComp.generateMaterial();
                 
         }
