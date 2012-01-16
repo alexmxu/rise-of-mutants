3998a8fe3f0be9753ff47545cc1029643c253adc
killa <killa@COLOSSUS>
2012-01-16 15:40:26 +0100

    ADD an example class to set up a SimpleApp class CLEAN code in the entity system classes - node component is attached to the scene directly when created - object tag becomes private

--- dev/trunk/TechDemo/src/com/bigboots/BBMain.java
+++ dev/trunk/TechDemo/src/com/bigboots/BBMain.java
@@ -1,11 +1,30 @@
 package com.bigboots;
 
-/**
- *
- * @author @author Ulrich Nzuzi <ulrichnz@code.google.com>
- */
+//import com.bigboots.states.BBInGameState;
+import com.bigboots.gui.BBGuiManager;
+import com.bigboots.states.BBMainMenuState;
+import com.bigboots.states.BBStateManager;
+
 
 public class BBMain extends BBApplication{
+
+    @Override
+    public void simpleInitialize(){
+        //Set up interface gui XML
+        BBGuiManager.getInstance().addXmlGui("Interface/mainmenu.xml");
+        BBGuiManager.getInstance().addXmlGui("Interface/ingame.xml");
+        BBGuiManager.getInstance().getNifty().gotoScreen("null");
+        
+        //Launch the main menu screen
+        BBMainMenuState window = new BBMainMenuState();
+        //BBInGameState window = new BBInGameState();
+        BBStateManager.getInstance().attach(window);
+    }
+    
+    @Override
+    public void simpleUpdate(){
+        
+    }    
     
     public static void main(String[] args) {
                
