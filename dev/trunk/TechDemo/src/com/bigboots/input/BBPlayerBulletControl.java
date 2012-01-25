3e2485670573e877d8f5759dd05a426cb0ca92c4
mifth <mifth@mifth-System-Product-Name>
2012-01-25 17:13:16 -0500

    BULLETS ARE BACK! Without physics! Works very fast. But need tobe collidable with entities. Some refactoring will need to be done.

--- dev/trunk/TechDemo/src/com/bigboots/input/BBPlayerBulletControl.java
+++ dev/trunk/TechDemo/src/com/bigboots/input/BBPlayerBulletControl.java
@@ -1,10 +1,16 @@
 package com.bigboots.input;
 
+import com.bigboots.components.BBAudioComponent;
 import com.bigboots.core.BBSceneManager;
 import com.bigboots.input.BBPlayerActions;
 import com.jme3.bounding.BoundingVolume;
 import com.jme3.collision.CollisionResults;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh.Type;
+import com.jme3.effect.shapes.EmitterSphereShape;
 import com.jme3.export.Savable;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
@@ -16,21 +22,28 @@ import com.jme3.scene.control.Control;
 
 public class BBPlayerBulletControl extends AbstractControl implements Savable, Cloneable{
     
-    BBPlayerActions sbc;
-    Geometry geooMove;
-    Transform bulletTrans;
-    float timer2;
-    Vector3f frontVec;
-    boolean work = true;
-    BoundingVolume bv;
-    
+    private BBPlayerActions sbc;
+    private Geometry geooMove;
+    private Transform bulletTrans;
+    private float timer2;
+    private Vector3f frontVec;
+    private boolean work = true;
+    private BoundingVolume bv;
+    private ParticleEmitter effect;
+    private BBAudioComponent expSound;    
     
     BBPlayerBulletControl (Geometry arg1, BBPlayerActions arg2){
-        //spaaMove = arg0;
+
         geooMove = arg1;
         sbc = arg2;
-     //   geooMove.setLocalTransform(bulletTrans);
 
+        prepareEffect();
+        expSound = new BBAudioComponent();
+        expSound.setSoundName("Sounds/explosionLarge2.ogg", false);
+        expSound.setLooping(false);
+        expSound.setVolume(10);        
+        
+        
 //Approach 1        
 // frontVec = geooMove.getLocalRotation().getRotationColumn(2).normalize();        
 
@@ -38,6 +51,33 @@ public class BBPlayerBulletControl extends AbstractControl implements Savable, C
 frontVec = geooMove.getWorldRotation().mult(Vector3f.UNIT_Z).normalize();
 
     }
+    
+    
+    private void prepareEffect() {
+        int COUNT_FACTOR = 1;
+        float COUNT_FACTOR_F = 1f;
+        effect = new ParticleEmitter("Flame", Type.Triangle, 32 * COUNT_FACTOR);
+        effect.setSelectRandomImage(true);
+        effect.setStartColor(new ColorRGBA(1f, 0.4f, 0.05f, (float) (1f / COUNT_FACTOR_F)));
+        effect.setEndColor(new ColorRGBA(.4f, .22f, .12f, 0f));
+        effect.setStartSize(1.3f);
+        effect.setEndSize(2f);
+        effect.setShape(new EmitterSphereShape(Vector3f.ZERO, 1f));
+        effect.setParticlesPerSec(0);
+        effect.setGravity(0, -5f, 0);
+        effect.setLowLife(.4f);
+        effect.setHighLife(.5f);
+        effect.setImagesX(2);
+        effect.setImagesY(2);
+        Material mat = new Material(BBSceneManager.getInstance().getAssetManager(), "Common/MatDefs/Misc/Particle.j3md");
+        mat.setTexture("Texture", BBSceneManager.getInstance().getAssetManager().loadTexture("Effects/Explosion/flame.png"));
+        mat.setReceivesShadows(false);
+        effect.setMaterial(mat);
+        effect.setEnabled(false);
+    }    
+
+    
+    
     
     @Override
     protected void controlUpdate(float tpf) {
@@ -54,35 +94,59 @@ frontVec = geooMove.getWorldRotation().mult(Vector3f.UNIT_Z).normalize();
 // Approach 2
 geooMove.move(frontVec.mult(30f*tpf));
 
-//        bv = geooMove.getWorldBound();
-//        CollisionResults results = new CollisionResults();
-//        sbc.shoBuRu.enemyNode.collideWith(bv, results);
-//        
-//        if (results.size() > 0) {
-//        Spatial closest = results.getCollision(0).getGeometry();
-//        
-//        if(closest != null) {
-//
-//            closest.removeFromParent();
-//            geooMove.removeControl(this);
-//            sbc.shotsGroup.detachChild(geooMove);
-//            geooMove = null;
-//            work = false;
-//        }
-//                       
-//        }
+        bv = geooMove.getWorldBound();
+        CollisionResults results = new CollisionResults();
+        BBSceneManager.getInstance().getRootNode().collideWith(bv, results);
+        
+        if (results.size() > 0) {
+        Spatial closest = results.getClosestCollision().getGeometry();
+        
+        if(closest != null && (closest.getParent().getName().indexOf("base") == 0 
+           || closest.getParent().getName().indexOf("E") == 0)) {
 
-        
-        if (timer2 > 2.5f) {
-        
-            geooMove.removeControl(this);
+            geooMove.removeFromParent();
+
+
+
+            effect.setLocalTranslation(geooMove.getWorldTranslation());
+            BBSceneManager.getInstance().getRootNode().attachChild(effect);
+            effect.setEnabled(true);
+            effect.emitAllParticles();
+            expSound.play();
+
+            
+
+            //if destroy control meshes
+            if (timer2 > 2.5f) {
             BBSceneManager.getInstance().getRootNode().detachChild(geooMove);
-
+            BBSceneManager.getInstance().getRootNode().detachChild(effect);
+            effect.setEnabled(false);
+            effect = null;
+            expSound.stop();
+            expSound.destroy();
+            expSound = null;
+            geooMove.removeControl(this);
             geooMove = null;
             work = false;
-            
+
+         }
         }
-        }
+       }
+
+        // If bullet did not collided to a collidable object it destroys automatically
+        if (timer2 > 2.7f) {
+
+            BBSceneManager.getInstance().getRootNode().detachChild(geooMove);            
+            effect.setEnabled(false);
+            effect = null;
+            expSound.stop();
+            expSound.destroy();
+            expSound = null;
+            geooMove.removeControl(this);
+            geooMove = null;
+            work = false;
+          }
+         }
         }
 
     
