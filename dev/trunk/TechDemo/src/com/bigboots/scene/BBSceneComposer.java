e7dffaba5dffe31c4123fd78b475e1421d222d92
mifth <mifth@mifth-System-Product-Name>
2012-02-25 04:20:53 +0400

    Scene composer BIG FIX3.

--- dev/trunk/TechDemo/src/com/bigboots/scene/BBSceneComposer.java
+++ dev/trunk/TechDemo/src/com/bigboots/scene/BBSceneComposer.java
@@ -65,6 +65,10 @@ public class BBSceneComposer {
 
         startCompose();
         
+        alNodesOriginals.clear();
+        alEntitiesOriginals.clear();
+        alEntitiesClones.clear();        
+        
     }
 
 
